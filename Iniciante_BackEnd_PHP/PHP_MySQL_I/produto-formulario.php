e8a19bd03925342e190fb53ce7813738661b0e2a
Fernanda Aparecida Ferreira <fromnanda@users.noreply.github.com>
2017-08-29 11:40:58 -0300

    Update produto-formulario.php

--- Iniciante_BackEnd_PHP/PHP_MySQL_I/produto-formulario.php
+++ Iniciante_BackEnd_PHP/PHP_MySQL_I/produto-formulario.php
@@ -1,15 +1,4 @@
-<html>
-<head>
-    <title>Minha loja</title>
-    <meta charset="utf-8">
-    <link href="css/bootstrap.css" rel="stylesheet" />
-    <link href="css/loja.css" rel="stylesheet" />
-</head>
-
-<body>
-    <div class="container">
-
-        <div class="principal">
+<?php include("cabecalho.php"); ?>
 
         <h1>Formulário de cadastro</h1>
         <form action="adiciona-produto.php">
@@ -19,9 +8,4 @@
             <input type="submit" value="Cadastrar" />
         </form>
 
-        </div>
-
-    </div>
-
-</body>
-</html>
+<?php include("rodape.php"); ?>
