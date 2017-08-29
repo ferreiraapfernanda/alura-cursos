a165a17c32a630263a13eef4897ba6926a78b544
Fernanda Aparecida Ferreira <fromnanda@users.noreply.github.com>
2017-08-29 11:40:03 -0300

    Update produto-formulario.php

--- Iniciante_BackEnd_PHP/PHP_MySQL_I/produto-formulario.php
+++ Iniciante_BackEnd_PHP/PHP_MySQL_I/produto-formulario.php
@@ -1,8 +1,27 @@
 <html>
-    <form action="adiciona-produto.php">
-        Nome: <input type="text" name="nome" /><br/>
-        Preço: <input type="number" name="preco" /><br/>
+<head>
+    <title>Minha loja</title>
+    <meta charset="utf-8">
+    <link href="css/bootstrap.css" rel="stylesheet" />
+    <link href="css/loja.css" rel="stylesheet" />
+</head>
 
-        <input type="submit" value="Cadastrar" />
-    </form>
+<body>
+    <div class="container">
+
+        <div class="principal">
+
+        <h1>Formulário de cadastro</h1>
+        <form action="adiciona-produto.php">
+            Nome: <input type="text" name="nome" /><br/>
+            Preço: <input type="number" name="preco" /><br/>
+
+            <input type="submit" value="Cadastrar" />
+        </form>
+
+        </div>
+
+    </div>
+
+</body>
 </html>
