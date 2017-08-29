18cd77b4d5d65f19b7e1feaa6443109bc5c41191
Fernanda Aparecida Ferreira <fromnanda@users.noreply.github.com>
2017-08-29 11:36:39 -0300

    Update index.php

--- Iniciante_BackEnd_PHP/PHP_MySQL_I/index.php
+++ Iniciante_BackEnd_PHP/PHP_MySQL_I/index.php
@@ -1,3 +1,19 @@
 <html>
-<h1> Bem vindo! </h1>
+<head>
+    <title>Minha loja</title>
+    <meta charset="utf-8">
+    <link href="css/bootstrap.css" rel="stylesheet" />
+    <link href="css/loja.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="principal">
+            <h1>Bem vindo!</h1>
+        </div>
+
+    </div>
+
+</body>
 </html>
