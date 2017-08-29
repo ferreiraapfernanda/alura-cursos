543e2c1db9ecebf8ebc708a6ebbacd52ed057e03
Fernanda Aparecida Ferreira <fromnanda@users.noreply.github.com>
2017-08-29 11:36:09 -0300

    Create produto-formulario.php

---
+++ Iniciante_BackEnd_PHP/PHP_MySQL_I/produto-formulario.php
@@ -0,0 +1,8 @@
+<html>
+    <form action="adiciona-produto.php">
+        Nome: <input type="text" name="nome" /><br/>
+        Preço: <input type="number" name="preco" /><br/>
+
+        <input type="submit" value="Cadastrar" />
+    </form>
+</html>
