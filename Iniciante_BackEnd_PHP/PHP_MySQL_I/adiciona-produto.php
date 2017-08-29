86c55d3a1aa88ca6149b45f5e2b482a61b43b8de
Fernanda Aparecida Ferreira <fromnanda@users.noreply.github.com>
2017-08-29 11:42:47 -0300

    Update adiciona-produto.php

--- Iniciante_BackEnd_PHP/PHP_MySQL_I/adiciona-produto.php
+++ Iniciante_BackEnd_PHP/PHP_MySQL_I/adiciona-produto.php
@@ -1,28 +1,11 @@
-<html>
-<head>
-    <title>Minha loja</title>
-    <meta charset="utf-8">
-    <link href="css/bootstrap.css" rel="stylesheet" />
-    <link href="css/loja.css" rel="stylesheet" />
-</head>
-
-<body>
-    <div class="container">
-
-        <div class="principal">
-
-        <?php
-        $nome = $_GET["nome"];
-        $preco = $_GET["preco"];
-        ?>
+<?php include("cabecalho.php"); ?>
+<?php
+$nome = $_GET["nome"];
+$preco = $_GET["preco"];
+?>
         
-        <p class="alert-success">
-         Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!
-        </p>
+	<p class="alert-success">
+		Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!
+	</p>
 
-        </div>
-
-    </div>
-
-</body>
-</html>
+<?php include("rodape.php"); ?>
