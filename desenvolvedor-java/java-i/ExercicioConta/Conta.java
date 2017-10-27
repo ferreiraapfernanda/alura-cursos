680a38c2e60af799cb3f19a8435d6776c1313b43
Fernanda Aparecida <ferreira.aparecidaf@gmail.com>
2017-10-27 13:12:56 -0200

    Java I: Começando Aula 05

---
+++ desenvolvedor-java/java-i/ExercicioConta/Conta.java
@@ -0,0 +1,81 @@
+class Conta {
+
+    int numero;
+    Pessoa titular;
+    private double saldo;
+    int agencia;
+    private double limite;
+
+    public double getSaldo(){
+        return this.saldo;
+    }
+
+    public void setLimite(double limite){
+        this.limite = limite;
+    }
+
+    public void deposita(double valorASerDepositavo) {
+        this.saldo += valorASerDepositavo;
+    }
+
+    public void saca(double valorASerSacado) {
+
+        if (valorASerSacado > this.saldo + this.limite) {
+            System.out.println("Saque invalido");
+        } else {
+            this.saldo -= valorASerSacado;
+        }
+    }
+
+    public void transfere(double valor, Conta destino) {
+        this.saldo -= valor;
+        destino.saldo += valor;
+    }
+
+}
+
+class Pessoa {
+    String nome;
+    String cpf;
+    String dataDeNascimento;
+
+    String endereco;
+}
+
+/*
+class Programa {
+
+    public static void main(String[] args) {
+
+        Conta mauricio = new Conta();
+        mauricio.numero = 123;
+        mauricio.saldo = 800.0;
+        mauricio.agencia = 842;
+
+        mauricio.titular = new Pessoa();
+        mauricio.titular.nome = "Mauricio Aniche";
+        mauricio.titular.cpf = "123456789";
+        mauricio.titular.dataDeNascimento = "10/10/2010";
+
+        System.out.println(mauricio.titular.nome);
+
+        mauricio.deposita(100.0);
+        mauricio.saca(5000.0);
+
+        Conta guilherme = new Conta();
+        guilherme.numero = 456;
+        guilherme.saldo = 1200.0;
+
+        guilherme.deposita(100.0);
+
+        mauricio.saca(100.0);
+        guilherme.deposita(100.0);
+
+        mauricio.transfere(100.0, guilherme);
+
+        System.out.println("Saldo Mauricio: " + mauricio.saldo);
+        System.out.println("Saldo Guilherme: " + guilherme.saldo);
+    }
+}
+
+*/
---
+++ desenvolvedor-java/java-i/ExercicioConta/TesteModificador.java
@@ -0,0 +1,13 @@
+class TesteModificador {
+
+    public static void main(String[] args) {
+        Conta joao = new Conta();
+        joao.numero = 123;
+        joao.setLimite(100);
+
+        joao.deposita(500);
+        joao.saca(600);
+
+        System.out.println(joao.getSaldo());
+    }
+}
